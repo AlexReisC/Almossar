bdede41247d4779f91f2ea6c400f198c0752556f
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-10-06 00:47:17 -0300

    feat: buscar cardapio por horario

--- src/main/java/br/edu/ufca/chatbot_UFCA/bot/Comandos.java
+++ src/main/java/br/edu/ufca/chatbot_UFCA/bot/Comandos.java
@@ -1,8 +1,15 @@
-package br.edu.ufca.chatbot_UFCA.utils;
+package br.edu.ufca.chatbot_UFCA.bot;
+
+		import java.io.IOException;
+		import java.time.LocalDate;
+		import java.time.LocalTime;
+		
+		import br.edu.ufca.chatbot_UFCA.downloader.PdfDownloader;
+		import br.edu.ufca.chatbot_UFCA.extractor.PdfExtractor;
 
 public class Comandos {
 	
-	public String exibirComandos(String comando) {
+	public static String exibirComandos(String comando) {
 		switch (comando) {
 		case "start":
 			return "JÁ PODI ALMOSSAR?\n" + 
@@ -13,7 +20,7 @@ public class Comandos {
 				"❓ /ajuda - Listar os comandos disponíveis \n" +
 				"📲 /contato - Entrar em contato com o criador";
 		case "cardapio":
-			return "";
+			return obterCardapio(); 
 		case "horarios":
 			return "Horários de funcionamento do Restaurante Universitário da UFCA: \n" + 
 					"☀️ Almoço: 11h - 14h (Juazeiro do Norte)\n" + 
@@ -32,12 +39,54 @@ public class Comandos {
 					"📲 /contato - Entrar em contato com o criador\n" +
 					"❓ /ajuda - Listar os comandos disponíveis";
 		case "contato":
-			return "Este bot foi desenvolvido por Alex Reis. Se tiver dúvidas, sugestões ou quer reportar um problema use:\n" +
+			return "Este bot foi desenvolvido por Alex Reis. Se tiver dúvidas, sugestões de melhorias ou quer reportar um problema:\n" +
 					"📷 Instagram: @c_alexreis \n" +
 					"📧 Email: alex.reis@aluno.ufca.edu.br \n" + 
 					"🌐 Linkedin: https://linkedin.com/in/alex-reis-cavalcante \n";
 		default:
 			return "Não entendi, este comando. Digite '/ajuda' para listar os comandos disponíveis.";
 		}
+	}
+	
+	public static String obterCardapio(){
+		LocalDate hoje = LocalDate.now();
+		int dia = hoje.getDayOfWeek().getValue();
+		dia = 5; 
+		if(dia > 5) {
+			return "Sem cardápio para hoje.";
+		}
+		
+		int diaDoMes = hoje.getDayOfMonth();
+		hoje.getDayOfWeek().toString();
+		int mes = hoje.getMonthValue();
+		int ano = hoje.getYear();
+		
+		try {
+			PdfExtractor.extrairTextoDia(PdfDownloader.NOME_ARQUIVO, dia);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String cabecalho = "Cardápio de " + diaDoMes + "/" + mes + "/" + ano;
+		
+		StringBuilder refeicao = new StringBuilder();		
+		
+		boolean isAlmoco = LocalTime.now().getHour() < 14 ? true : false;
+		if(!isAlmoco) {
+			for(StringBuilder stringBuilder : PdfExtractor.jantarDoDia) {
+				refeicao.append(stringBuilder.toString());
+			}
+			return cabecalho + "\nJantar\n" + refeicao.toString() + 
+					"\n\n* Contém leite/lactose/glúten\n" + 
+					"OBS: O cardápio pode sofrer alterações.\n" +
+					"(O CARDAPIO DO JANTAR É DISPONIBILIZADO AQUI APÓS ÀS 14:00 HORAS)";
+		}
+		for(StringBuilder stringBuilder : PdfExtractor.almocoDoDia) {
+			refeicao.append(stringBuilder.toString());
+		}
+		return cabecalho + "\nAlmoço\n" + refeicao.toString() + 
+				"\n\n* Contém leite/lactose/glúten\n" + 
+				"OBS: O cardápio pode sofrer alterações.\n" +
+				"(O CARDAPIO DO JANTAR É DISPONIBILIZADO AQUI APÓS ÀS 14:00 HORAS)";		
 	}
 }
