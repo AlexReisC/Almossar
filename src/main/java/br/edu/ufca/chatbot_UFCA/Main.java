98ece2d8bc266b317dc6886808519031d99a2e6d
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-10-06 00:55:06 -0300

    classe de teste

--- src/main/java/br/edu/ufca/chatbot_UFCA/Main.java
+++ src/main/java/br/edu/ufca/chatbot_UFCA/Main.java
@@ -1,26 +1,19 @@
 package br.edu.ufca.chatbot_UFCA;
 
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import br.edu.ufca.chatbot_UFCA.downloader.PdfDownloader;
-import br.edu.ufca.chatbot_UFCA.extractor.PdfExtractor;
+import br.edu.ufca.chatbot_UFCA.bot.Comandos;
+import br.edu.ufca.chatbot_UFCA.utils.JobScheduler;
 
 public class Main 
 {
 	private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main( String[] args )
     {
-//    	try {
-////			PdfDownloader.baixarPdf(PdfDownloader.ufcaSite);
-//    		PdfExtractor.extrairTexto(PdfDownloader.nomeArquivo);
-//    	} catch (IOException e) {
-//			logger.info("Erro ao tentar ler pdf. {}", e);
-//		}
-    	String prato = "\\uD83D\\uDE00";
-    	System.out.println(prato + " /cardapio - Ver o cardápio do dia. \\uD83D\\uDE00");
+//    	JobScheduler jobScheduler = new JobScheduler();
+//    	jobScheduler.agendarDownload();
+    	Comandos cmd = new Comandos();
+    	System.out.println(cmd.exibirComandos("cardapio"));
     }
 }
