e7921f8e565f292f9cffede5a4bcf5d79fede201
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-10-11 19:35:53 -0300

    agendar envido do cardapio

--- src/main/java/br/edu/ufca/chatbot_UFCA/utils/JobScheduler.java
+++ src/main/java/br/edu/ufca/chatbot_UFCA/utils/JobScheduler.java
@@ -1,7 +1,6 @@
 package br.edu.ufca.chatbot_UFCA.utils;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.quartz.CronScheduleBuilder;
 import org.quartz.DailyTimeIntervalScheduleBuilder;
 import org.quartz.DateBuilder;
 import org.quartz.JobBuilder;
@@ -13,16 +12,17 @@ import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
 
+import br.edu.ufca.chatbot_UFCA.bot.SendDailyCardapio;
+import br.edu.ufca.chatbot_UFCA.downloader.PdfDeleter;
 import br.edu.ufca.chatbot_UFCA.downloader.PdfDownloader;
 
 public class JobScheduler {
-	private static final Logger logger = LogManager.getLogger(JobScheduler.class);
-	
-	public void agendarDownload() {
+
+	public void agendarDownload() throws SchedulerException {
 		JobDetail jobDetail = JobBuilder.newJob(PdfDownloader.class).withIdentity("pdfDownloadJob", "grupo").build();
 
 		TimeOfDay inicio = TimeOfDay.hourAndMinuteOfDay(8, 0);
-		TimeOfDay fim = TimeOfDay.hourAndMinuteOfDay(11, 30);
+		TimeOfDay fim = TimeOfDay.hourAndMinuteOfDay(11, 0);
 
 		DailyTimeIntervalScheduleBuilder dailySchedule = DailyTimeIntervalScheduleBuilder.dailyTimeIntervalSchedule()
 				.startingDailyAt(inicio).endingDailyAt(fim).withIntervalInMinutes(15)
@@ -32,12 +32,45 @@ public class JobScheduler {
 				.withSchedule(dailySchedule).build();
 
 		Scheduler scheduler;
-		try {
-			scheduler = StdSchedulerFactory.getDefaultScheduler();
-			scheduler.scheduleJob(jobDetail, trigger);
-			scheduler.start();
-		} catch (SchedulerException e) {
-			logger.info("Job ou Trigger nao adicionado. {}", e);
-		}
+		scheduler = StdSchedulerFactory.getDefaultScheduler();
+		scheduler.scheduleJob(jobDetail, trigger);
+		scheduler.start();
+		scheduler.pauseAll();
 	}
+	
+	public void deletarPdf() throws SchedulerException {
+		JobDetail deletionJobDetail = JobBuilder.newJob(PdfDeleter.class).withIdentity("pdfDeletionJob", "grupo").build();
+
+		Trigger deletionTrigger = TriggerBuilder.newTrigger().withIdentity("pdfDeletionTrigger", "grupo")
+				.withSchedule(CronScheduleBuilder.weeklyOnDayAndHourAndMinute(DateBuilder.SATURDAY, 0, 0)).build();
+
+		Scheduler scheduler;
+		scheduler = StdSchedulerFactory.getDefaultScheduler();
+		scheduler.scheduleJob(deletionJobDetail, deletionTrigger);
+		scheduler.start();
+	}
+	
+	public void agendarEnvioDiario() throws SchedulerException{
+        JobDetail jobDetail = JobBuilder.newJob(SendDailyCardapio.class)
+                                        .withIdentity("sendDailyMenuJob", "grupo")
+                                        .build();
+        
+        TimeOfDay inicio = TimeOfDay.hourAndMinuteOfDay(8, 1);
+        TimeOfDay fim = TimeOfDay.hourAndMinuteOfDay(14, 2);
+        DailyTimeIntervalScheduleBuilder dailyTime = DailyTimeIntervalScheduleBuilder.dailyTimeIntervalSchedule()
+        								.startingDailyAt(inicio)
+        								.endingDailyAt(fim)
+        								.withIntervalInHours(6)
+        								.onEveryDay();
+        
+        Trigger trigger = TriggerBuilder.newTrigger()
+                                        .withIdentity("dailyMenuTrigger", "grupo")
+                                        .withSchedule(dailyTime)
+                                        .build();
+
+        Scheduler scheduler;
+		scheduler = StdSchedulerFactory.getDefaultScheduler();
+		scheduler.scheduleJob(jobDetail, trigger);
+		scheduler.start();
+    }
 }
