9a333da2ff8a0cc65bcb6ddd116ddb639b400105
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-10-12 23:02:39 -0300

    refactor: remove classe Run

--- src/main/java/br/edu/ufca/chatbot_UFCA/Main.java
+++ src/main/java/br/edu/ufca/chatbot_UFCA/Main.java
@@ -1,12 +1,36 @@
 package br.edu.ufca.chatbot_UFCA;
 
-import br.edu.ufca.chatbot_UFCA.run.Run;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.SchedulerException;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import br.edu.ufca.chatbot_UFCA.bot.CardapioBot;
+import br.edu.ufca.chatbot_UFCA.utils.JobScheduler;
 
 public class Main 
 {
-    public static void main( String[] args )
-    {
-    	Run.run();
+	private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main( String[] args ) {
+    	String botToken = System.getenv("CARDAPIO_TOKEN_BOT");
+		TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+		
+		JobScheduler jobScheduler = new JobScheduler();
+		try {
+			jobScheduler.deletarPdf();
+			jobScheduler.agendarDownload();
+
+			botsApplication.registerBot(botToken, new CardapioBot(botToken));
+			jobScheduler.agendarEnvioDiario();
+			Thread.currentThread().join();
+		} catch (SchedulerException e) {
+			logger.info("Erro ao agendar jobs na classe JobScheduler. {}", e);
+		} catch (TelegramApiException e2) {
+			logger.info("Erro ao registrar bot {}", e2);
+		} catch (InterruptedException e3) {
+			logger.info("Thread atual interrompida {}", e3);
+		}
     	
     }
 }
